7b8e48876bcb59381325129ff8ebee989c103f0b
Dung Nguyen <dungnguyen310703@gmail.com>
2026-03-31 17:24:53 +0700

    add ADK Agent, warp LangGraph Multi Agent System

--- src/a2a_mcp/__main__.py
+++ src/a2a_mcp/__main__.py
@@ -8,7 +8,6 @@ from a2a_mcp.mcp import server
 @click.option('--port','port', default=10000)
 @click.option('--transport','transport', default='streamable-http')
 def main(command, host, port, transport) -> None:
-    # TODO: Add other servers, perhaps dynamic port allocation
     if command == 'mcp-server':
         server.serve(host, port, transport)
     else:
