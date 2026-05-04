fcc97535756ba7fcfbaf07ea5789e8f1770be171
DGuz317 <dungnguyen310703@gmail.com>
2026-05-04 10:31:31 +0700

    Connect to MCP Server, Add Logging Debug, Add Strict recall tools

--- src/agent_app/agents/invoice_agent_2.py
+++ src/agent_app/agents/invoice_agent_2.py
@@ -1,103 +1,45 @@
 import os
 import ast 
+import asyncio
+# import logging
+# import colorlog
 from dotenv import load_dotenv
 
 from pydantic import BaseModel, Field
 from typing import Literal, List, Optional, Dict, Any
 
-from langchain_community.utilities import SQLDatabase
+# from langchain_community.utilities import SQLDatabase
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain.agents import create_agent
-from langchain.tools import tool, ToolRuntime
+# from langchain.tools import tool, ToolRuntime
 from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import InMemorySaver
-from langchain_ollama import ChatOllama
+# from langchain_ollama import ChatOllama
 
 from agent_app.common.prompts import INVOICE_AGENT_PROMPT
 
 
 load_dotenv()
 checkpointer = InMemorySaver()
+# handler = colorlog.StreamHandler()
+# handler.setFormatter(colorlog.ColoredFormatter(
+#     "%(log_color)s%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+#     log_colors={
+#         "DEBUG": "cyan",
+#         "INFO": "green",
+#         "WARNING": "yellow",
+#         "ERROR": "red",
+#         "CRITICAL": "bold_red",
+#     }
+# ))
+
+# logger = colorlog.getLogger(__name__)
+# logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
 
 
-# --- Databases ---
-db = SQLDatabase.from_uri("sqlite:////home/nvdung1/Desktop/MultiAgentSystem/langgraph-agent/src/agent_app/database/chinook.db")
-
-# --- Tools ---
-@tool()
-def get_invoices_by_customer_sorted_by_date(customer_id: str) -> list[dict]:
-    """
-    Look up all invoices for a customer using their ID.
-    The invoices are sorted in descending order by invoice date, which helps when the customer wants to view their most recent/oldest invoice, or if 
-    they want to view invoices within a specific date range.
-    
-    Args:
-        customer_id (str): customer_id, which serves as the identifier.
-    
-    Returns:
-        list[dict]: A list of invoices for the customer.
-    """
-    result = db.run(
-        f"SELECT * FROM Invoice WHERE CustomerId = {customer_id} ORDER BY InvoiceDate DESC;",
-        include_columns=True
-    )
-    if not result:
-        return []
-    return ast.literal_eval(result)
-
-@tool()
-def get_invoices_sorted_by_unit_price(customer_id: str) -> list[dict]:
-    """
-    Use this tool when the customer wants to know the details of one of their invoices based on the unit price/cost of the invoice.
-    This tool looks up all invoices for a customer, and sorts the unit price from highest to lowest. In order to find the invoice associated with the customer, 
-    we need to know the customer ID.
-    
-    Args:
-        customer_id (str): customer_id, which serves as the identifier.
-    
-    Returns:
-        list[dict]: A list of invoices sorted by unit price.
-    """
-    query = f"""
-        SELECT Invoice.*, InvoiceLine.UnitPrice
-        FROM Invoice
-        JOIN InvoiceLine ON Invoice.InvoiceId = InvoiceLine.InvoiceId
-        WHERE Invoice.CustomerId = {customer_id}
-        ORDER BY InvoiceLine.UnitPrice DESC;
-    """
-    result = db.run(query, include_columns=True)
-    if not result:
-        return []
-    return ast.literal_eval(result)
-
-@tool()
-def get_employee_by_invoice_and_customer(invoice_id: str, customer_id: str) -> dict:
-    """
-    This tool will take in an invoice ID and a customer ID and return the employee information associated with the invoice.
-
-    Args:
-        invoice_id (int): The ID of the specific invoice.
-        customer_id (str): customer_id, which serves as the identifier.
-
-    Returns:
-        dict: Information about the employee associated with the invoice.
-    """
-    query = f"""
-        SELECT Employee.FirstName, Employee.Title, Employee.Email
-        FROM Employee
-        JOIN Customer ON Customer.SupportRepId = Employee.EmployeeId
-        JOIN Invoice ON Invoice.CustomerId = Customer.CustomerId
-        WHERE Invoice.InvoiceId = ({invoice_id}) AND Invoice.CustomerId = ({customer_id});
-    """
-    result = db.run(query, include_columns=True)
-    if not result:
-        return {"error": f"No employee found for invoice ID {invoice_id} and customer ID {customer_id}."}
-    
-    parsed = ast.literal_eval(result)
-    return parsed[0] if isinstance(parsed, list) else parsed
-
-# --- Connect MCP tools ---
-
-
+# --- Defined Response Format ---
 class ResponseFormat(BaseModel):
     """Response to user using this format"""
     status: Literal["input_required", "completed", "failed"] = Field(..., description="The current state of the agent's execution loop.")
@@ -108,29 +50,51 @@ class ResponseFormat(BaseModel):
 
 MODEL = init_chat_model(
     model=os.getenv("LLM_MODEL"),
-    model_provider="ollama",
+    model_provider=os.getenv("MODEL_PROVIDER"),
     base_url=os.getenv("OLLAMA_API_URL"),
     temperature=0,
     timeout=300,
     max_tokens=25000,
 )
 
-invoice_agent = create_agent(
-    model=MODEL,
-    tools=[
-        get_invoices_by_customer_sorted_by_date,
-        get_invoices_sorted_by_unit_price,
-        get_employee_by_invoice_and_customer
-    ],
-    system_prompt=INVOICE_AGENT_PROMPT,
-    response_format=ResponseFormat,
-    checkpointer=checkpointer,
-)
+# --- Allowed Tools ---
+ALLOWED_TOOL_NAMES = {
+    "get_invoices_by_customer_sorted_by_date",
+    "get_invoices_sorted_by_unit_price",
+    "get_employee_by_invoice_and_customer",
+}
+
+# --- Connect MCP Server ---
+async def invoice_agent_response():
+    client = MultiServerMCPClient(
+        {
+            "data_tools": {
+                    "transport": "streamable-http",
+                    "url": "http://localhost:10000/mcp"
+            }
+        }
+    )
+
+    async with client.session("data_tools") as session:
+        tools = await load_mcp_tools(session)
+        invoice_tools = [t for t in tools if t.name in ALLOWED_TOOL_NAMES]
+        # logger.info(f"Allowed tools from MCP Server: {invoice_tools}")
+        invoice_agent = create_agent(
+            model=MODEL,
+            tools=invoice_tools,
+            system_prompt=INVOICE_AGENT_PROMPT,
+            response_format=ResponseFormat,
+            checkpointer=checkpointer,
+        )
 
 # --- Simple test ---
-# agent_result = invoice_agent.invoke(
-#     {"messages": [{"role": "user", "content": "My customer id is 1. What is my latest invoice?"}]},
-#     config={"configurable": {"thread_id": "1"}},
-# )
+        agent_result = await invoice_agent.ainvoke(
+            {"messages": [{"role": "user", "content": "My customer id is 1. What is my latest invoice?"}]},
+            config={"configurable": {"thread_id": "1"}},
+        )
+        # logger.info(f"Agent Response:")
+        print(agent_result["messages"][-1].content_blocks)
 
-# print(agent_result["messages"][-1].content_blocks)
+
+if __name__ == "__main__":
+    asyncio.run(invoice_agent_response())
--- src/agent_app/agents/music_agent_2.py
+++ src/agent_app/agents/music_agent_2.py
@@ -1,136 +1,45 @@
 import os
-import ast 
+import ast
+import asyncio
+# import logging
+# import colorlog
 from dotenv import load_dotenv
 
 from pydantic import BaseModel, Field
 from typing import Literal, List, Optional, Dict, Any
 
-from langchain_community.utilities import SQLDatabase
+# from langchain_community.utilities import SQLDatabase
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain.agents import create_agent
-from langchain.tools import tool, ToolRuntime
+# from langchain.tools import tool, ToolRuntime
 from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import InMemorySaver
-from langchain_ollama import ChatOllama
+# from langchain_ollama import ChatOllama
 
 from agent_app.common.prompts import MUSIC_AGENT_PROMPT
 
 
 load_dotenv()
 checkpointer = InMemorySaver()
+# handler = colorlog.StreamHandler()
+# handler.setFormatter(colorlog.ColoredFormatter(
+#     "%(log_color)s%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+#     log_colors={
+#         "DEBUG": "cyan",
+#         "INFO": "green",
+#         "WARNING": "yellow",
+#         "ERROR": "red",
+#         "CRITICAL": "bold_red",
+#     }
+# ))
+
+# logger = colorlog.getLogger(__name__)
+# logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
 
 
-# --- Databases ---
-db = SQLDatabase.from_uri("sqlite:////home/nvdung1/Desktop/MultiAgentSystem/langgraph-agent/src/agent_app/database/chinook.db")
-
-# --- Tools ---
-@tool()
-def get_albums_by_artist(artist: str):
-    """
-    Get albums by an artist.
-    
-    Args:
-        artist (str): Name of the artist.
-
-    Returns:
-        list[dict]: A list of albums that match the artist name.
-    """
-    result = db.run(
-        f"""
-        SELECT Album.Title, Artist.Name 
-        FROM Album 
-        JOIN Artist ON Album.ArtistId = Artist.ArtistId 
-        WHERE Artist.Name LIKE '%{artist}%';
-        """,
-        include_columns=True
-    )
-    if not result:
-        return []
-    return ast.literal_eval(result)
-
-@tool()
-def get_tracks_by_artist(artist: str):
-    """
-    Get all songs for customer using artist name.
-    
-    Args:
-        artist (str): Name of the artist.
-
-    Returns:
-        list[dict]: A list of songs that match the artist name.
-    """
-    result = db.run(
-        f"""
-        SELECT Track.Name as SongName, Artist.Name as ArtistName 
-        FROM Album 
-        LEFT JOIN Artist ON Album.ArtistId = Artist.ArtistId 
-        LEFT JOIN Track ON Track.AlbumId = Album.AlbumId 
-        WHERE Artist.Name LIKE '%{artist}%';
-        """,
-        include_columns=True
-    )
-    if not result:
-        return []
-    return ast.literal_eval(result)
-
-@tool()
-def get_songs_by_genre(genre: str):
-    """
-    Fetch songs from the database that match a specific genre.
-    
-    Args:
-        genre (str): The genre of the songs to fetch.
-    
-    Returns:
-        list[dict]: A list of songs that match the specified genre.
-    """
-    genre_id_query = f"SELECT GenreId FROM Genre WHERE Name LIKE '%{genre}%'"
-    genre_ids = db.run(genre_id_query)
-    if not genre_ids:
-        return f"No songs found for the genre: {genre}"
-    genre_ids = ast.literal_eval(genre_ids)
-    genre_id_list = ", ".join(str(gid[0]) for gid in genre_ids)
-    songs_query = f"""
-        SELECT Track.Name as SongName, Artist.Name as ArtistName
-        FROM Track
-        LEFT JOIN Album ON Track.AlbumId = Album.AlbumId
-        LEFT JOIN Artist ON Album.ArtistId = Artist.ArtistId
-        WHERE Track.GenreId IN ({genre_id_list})
-        GROUP BY Artist.Name
-        LIMIT 8;
-    """
-    songs = db.run(songs_query, include_columns=True)
-    if not songs:
-        return f"No songs found for the genre: {genre}"
-    formatted_songs = ast.literal_eval(songs)
-    return [
-        {"Song": song["SongName"], "Artist": song["ArtistName"]}
-        for song in formatted_songs
-    ]
-
-@tool()
-def check_for_songs(song_title: str):
-    """
-    Check if a song exists by its name.
-    
-    Args:
-        song_title (str): Name of the song.
-
-    Returns:
-        list[dict]: A list of songs that match the song name.
-    """
-    result = db.run(
-        f"""
-        SELECT * FROM Track WHERE Name LIKE '%{song_title}%';
-        """,
-        include_columns=True
-    )
-    if not result:
-        return []
-    return ast.literal_eval(result)
-
-# --- Connect MCP tools ---
-
-
+# --- Defined Response Format ---
 class ResponseFormat(BaseModel):
     """Response to user using this format"""
     status: Literal["input_required", "completed", "failed"] = Field(..., description="The current state of the agent's execution loop.")
@@ -141,30 +50,51 @@ class ResponseFormat(BaseModel):
 
 MODEL = init_chat_model(
     model=os.getenv("LLM_MODEL"),
-    model_provider="ollama",
+    model_provider=os.getenv("MODEL_PROVIDER"),
     base_url=os.getenv("OLLAMA_API_URL"),
     temperature=0,
     timeout=300,
     max_tokens=25000,
 )
 
-invoice_agent = create_agent(
-    model=MODEL,
-    tools=[
-        get_albums_by_artist,
-        get_tracks_by_artist,
-        get_songs_by_genre,
-        check_for_songs
-    ],
-    system_prompt=MUSIC_AGENT_PROMPT,
-    response_format=ResponseFormat,
-    checkpointer=checkpointer,
-)
+# --- Allowed Tools ---
+ALLOWED_TOOL_NAMES = {
+    "get_albums_by_artist",
+    "get_tracks_by_artist",
+    "get_songs_by_genre",
+    "check_for_songs",
+}
+
+# --- Connect MCP Server ---
+async def music_agent_response():
+    client = MultiServerMCPClient(
+        {
+            "data_tools": {
+                    "transport": "streamable-http",
+                    "url": "http://localhost:10000/mcp"
+            }
+        }
+    )
+
+    async with client.session("data_tools") as session:
+        tools = await load_mcp_tools(session)
+        music_tools = [t for t in tools if t.name in ALLOWED_TOOL_NAMES]
+        # logger.info(f"Allowed tools from MCP Server: {music_tools}")
+        music_agent = create_agent(
+            model=MODEL,
+            tools=music_tools,
+            system_prompt=MUSIC_AGENT_PROMPT,
+            response_format=ResponseFormat,
+            checkpointer=checkpointer,
+        )
 
 # --- Simple test ---
-# agent_result = invoice_agent.invoke(
-#     {"messages": [{"role": "user", "content": "I like AC/DC. Can you recommend some of their tracks for me?"}]},
-#     config={"configurable": {"thread_id": "2"}},
-# )
+        agent_result = await music_agent.ainvoke(
+            {"messages": [{"role": "user", "content": "I like AC/DC. Can you recommend some of their tracks for me?"}]},
+            config={"configurable": {"thread_id": "2"}},
+        )
+        # logger.info(f"Agent Response:")
+        print(agent_result["messages"][-1].content_blocks)
 
-# print(agent_result["messages"][-1].content_blocks)
+if __name__ == "__main__":
+    asyncio.run(music_agent_response())
